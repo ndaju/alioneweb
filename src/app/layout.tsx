1615a17e7bd9a35ce9b587102a81c194200feb0a
ndaju <aiseciurty@gmail.com>
2026-06-26 19:27:06 +0300

    add proxyUrl=/__clerk on ClerkProvider, Nginx proxies /__clerk/ to clerk.auth.alione.cc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${outfit.variable} ${plusJakarta.variable}`}>
       <body>
-        <ClerkProvider>
+        <ClerkProvider proxyUrl="/__clerk">
           {children}
         </ClerkProvider>
       </body>
